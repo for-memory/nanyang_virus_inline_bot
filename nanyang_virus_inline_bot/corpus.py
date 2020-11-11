ad87a1768e8859906b6b23d853cdfbbac94e953a
for-memory <64400759+for-memory@users.noreply.github.com>
2020-11-11 23:04:41 +0800

    Update corpus.py

--- nanyang_virus_inline_bot/corpus.py
+++ nanyang_virus_inline_bot/corpus.py
@@ -5,7 +5,7 @@ from typing import List
 from httpx import AsyncClient, NetworkError, HTTPError
 from httpcore import TimeoutException
 
-BASE_URL = "https://raw.githubusercontent.com/Chi-Task-Force/Chi-Corpus/master"
+BASE_URL = "https://raw.githubusercontent.com/for-memory/nanyang_virus_inline_bot/master"
 
 
 class UpdateException(Exception):
@@ -15,8 +15,8 @@ class UpdateException(Exception):
 @dataclass
 class Corpus:
     common: List[str] = field(default_factory=list)
-    refuse: List[str] = field(default_factory=list)
-    trigger: List[str] = field(default_factory=list)
+    zhi: List[str] = field(default_factory=list)
+    na: List[str] = field(default_factory=list)
 
     def __post_init__(self):
         self.http = AsyncClient()
@@ -24,13 +24,13 @@ class Corpus:
     async def update(self):
         try:
             self.common = (await self.http.get(f"{BASE_URL}/common.txt")).text.strip().split("\n")
-            self.refuse = (await self.http.get(f"{BASE_URL}/refuse.txt")).text.strip().split("\n")
-            self.trigger = (await self.http.get(f"{BASE_URL}/trigger.txt")).text.strip().split("\n")
+            self.zhi = (await self.http.get(f"{BASE_URL}/zhi.txt")).text.strip().split("\n")
+            self.na = (await self.http.get(f"{BASE_URL}/na.txt")).text.strip().split("\n")
         except (NetworkError, HTTPError, TimeoutException) as e:
             raise UpdateException from e
 
     def get_rnd_common(self):
         return self.common[random.randint(0, len(self.common) - 1)]
 
-    def get_rnd_refuse(self):
-        return self.refuse[random.randint(0, len(self.refuse) - 1)]
+    def get_rnd_zhi(self):
+        return self.zhi[random.randint(0, len(self.zhi) - 1)]
